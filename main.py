b06f917bbfb8307c152943519448e88fbb425f62
kirill <k.nelyubov@gmail.com>
2024-12-11 20:02:56 +0300

    11/12

---
+++ main.py
@@ -0,0 +1,18 @@
+# 1st program
+
+print(9**0.5*5)
+
+# 2st program
+
+print(99.9 > 9.98 and 1000 != 1001)
+
+# 3st program
+
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+
+# 4st program
+
+a = '123.456'
+print(int(float(a)*10)%10)
